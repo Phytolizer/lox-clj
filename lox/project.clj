6adc7b748ce52cc54eeff46e5803cd6c7d8a2b99
Kyle Coffey <me@phytolizer.dev>
2022-11-29 22:05:27 -0600

    metadata

--- lox/project.clj
+++ lox/project.clj
@@ -1,8 +1,8 @@
 (defproject lox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Lox programming language"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot lox.core
   :target-path "target/%s"
