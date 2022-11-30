9a0ca85ce5c4313e65b678792d8109d12614a79a
Kyle Coffey <me@phytolizer.dev>
2022-11-29 22:02:50 -0600

    boilerplate

--- lox/src/lox/core.clj
+++ lox/src/lox/core.clj
@@ -1,7 +1,16 @@
 (ns lox.core
   (:gen-class))
 
+(defn run-prompt []
+  (println "lox> "))
+
+(defn run-file [file]
+  (println "Running file:" file))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (case (count args)
+    0 (run-prompt)
+    1 (run-file (first args))
+    (println "Usage: lox [script]")))
