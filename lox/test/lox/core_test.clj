6c834f18ea3366492b498292a8cc8b856734b6be
Kyle Coffey <me@phytolizer.dev>
2022-11-29 22:00:18 -0600

    fix imports of test

--- lox/test/lox/core_test.clj
+++ lox/test/lox/core_test.clj
@@ -1,6 +1,5 @@
 (ns lox.core-test
-  (:require [clojure.test :refer :all]
-            [lox.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
